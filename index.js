6c7f47697ad8921b3dff9dc900b2126154e65210
Louis Cassany <30923342+Cuke7@users.noreply.github.com>
2019-05-07 11:07:46 +0200

    Update index.js

--- index.js
+++ index.js
@@ -109,7 +109,7 @@ function handleMessage(sender_psid, received_message) {
 			let subtitle = "";
 			for (let j = 0; j < input.length; j++) {
 				console.log(results[1][j][i]);
-				subtitle = subtitle "\n" + "#" + input[j] + " = " + results[1][j][i].occ;
+				subtitle = subtitle + "\n" + "#" + input[j] + " = " + results[1][j][i].occ +" %";
 			}
 			response.attachment.payload.elements.push({
 				"title": title,
