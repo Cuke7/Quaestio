f9600e6e373e766c3bcc3a5d1a748b0329c7cdcd
Louis Cassany <30923342+Cuke7@users.noreply.github.com>
2019-05-07 10:55:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -108,7 +108,7 @@ function handleMessage(sender_psid, received_message) {
 			let title = decode(path)[0];
 			let subtitle = "";
 			for (let j = 0; j < input.length; j++) {
-				subtitle = subtitle + "\n" + "#" + input[j] + " = " + results[1][i].occ;
+				subtitle = subtitle + "\n" + "#" + input[j] + " = " + results[1][j][i].occ;
 			}
 			response.attachment.payload.elements.push({
 				"title": title,
